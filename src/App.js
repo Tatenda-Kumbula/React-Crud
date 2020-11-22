9e9b3832487c7f88b6fee7ba8611c665851f268c
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-22 08:39:41 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Products from './Products';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {        
     return (
       <div>
-        <h1>My First React App</h1>
         <Products />
+        <Button>Default</Button>
       </div>
     );
   }
