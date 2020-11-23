b6411c3f6124fe251a5e5d838f604cb3dfbca7b0
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-23 09:00:22 +0200

    Add files via upload

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,2 +1,8 @@
 import React, { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'Formik';
+
+class UserForm extends Component {
+    constructor(props){
+        super(props);
+    }
+}
