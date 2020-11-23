7aa986de7adf8bf00f3e9bdf583b2ba43cc7c6fc
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-23 08:43:38 +0200

    Add files via upload

--- src/JumboTronComponent.js
+++ src/JumboTronComponent.js
@@ -6,4 +6,19 @@ class JumbotronComponent extends Component {
     constructor(props){
         super(props);
     }
+
+render() {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>{this.props.children}</p>
+                <p><Button bStyle="primary">Learn more</Button></p>
+            </Jumbotron>
+        </div>
+    );
 }
+
+}
+
+export default JumboTronComponent;
