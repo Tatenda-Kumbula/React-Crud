814bafce0325868e4db4d1d8ae7cad876035963e
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-23 07:35:28 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import Products from './Products';
-import Rating from './Rating';
 
 class App extends Component {
   render() {        
     return (
       <div>
-        <Rating rating="1"/>
-        <Rating rating="2"/>
-        <Rating rating="3"/>
-        <Rating rating="4"/>
-        <Rating rating="5"/>
+        <Products />        
       </div>
     );
   }
