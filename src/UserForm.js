7c9ba30baad65483e02b5b52cd5c656d2c6bba62
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-23 09:01:18 +0200

    Add files via upload

--- src/UserForm.js
+++ src/UserForm.js
@@ -5,4 +5,23 @@ class UserForm extends Component {
     constructor(props){
         super(props);
     }
+    render(){
+        return(
+          <div>
+              <h1>Any place in your app!</h1>
+              <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={values => {
+                  let errors = {};
+                  if (!values.email) {
+                    errors.email = 'Required';
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                  ) {
+                    errors.email = 'Invalid email address';
+                  }
+                  else if (values.email.length < 10) {
+                    errors.email = 'Email address too short';
+                  } 
 }
+
