4c1e89b01704cf95b01d50778a82cdb7fdf20a27
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-23 06:49:01 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Products from './Products';
-import { Button } from 'react-bootstrap';
+import Rating from './Rating';
 
 class App extends Component {
   render() {        
     return (
       <div>
-        <Products />
-        <Button>Default</Button>
+        <Rating rating="1"/>
+        <Rating rating="2"/>
+        <Rating rating="3"/>
+        <Rating rating="4"/>
+        <Rating rating="5"/>
       </div>
     );
   }
