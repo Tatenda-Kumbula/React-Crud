cf74b58ae716fd4d192a347b974aaaebc40146c1
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-23 06:37:32 +0200

    Add files via upload

--- src/Rating.js
+++ src/Rating.js
@@ -11,3 +11,38 @@ class Rating extends Component {
 handleClick(ratingValue){
     this.setState({rating:ratingValue})
 }
+
+render() { 
+    return (
+      <div>
+        {this.state.rating >= 1 ? ( 
+            <IoIosStar onClick={this.handleClick.bind(this,1)}/>     
+        ) : (         
+            <IoIosStarOutline onClick={this.handleClick.bind(this,1)}/>     
+        )}     
+        {this.state.rating >= 2 ? ( 
+            <IoIosStar onClick={this.handleClick.bind(this,2)}/>     
+        ) : (         
+            <IoIosStarOutline onClick={this.handleClick.bind(this,2)}/>     
+        )} 
+        {this.state.rating >= 3 ? ( 
+            <IoIosStar onClick={this.handleClick.bind(this,3)}/>     
+        ) : (         
+            <IoIosStarOutline onClick={this.handleClick.bind(this,3)}/>     
+        )} 
+        {this.state.rating >= 4 ? ( 
+            <IoIosStar onClick={this.handleClick.bind(this,4)}/>     
+        ) : (         
+            <IoIosStarOutline onClick={this.handleClick.bind(this,4)}/>     
+        )} 
+        {this.state.rating >= 5 ? ( 
+            <IoIosStar onClick={this.handleClick.bind(this,5)}/>     
+        ) : (         
+            <IoIosStarOutline onClick={this.handleClick.bind(this,5)}/>     
+        )}                                                                                                                                  
+      </div>
+    );
+  }
+}
+
+export default Rating;
