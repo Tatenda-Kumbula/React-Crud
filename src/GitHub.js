a8e488e75acde8becebbf59530f97073205b98e6
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-24 15:24:49 +0200

    Add files via upload

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
+import { Media, Form, Button } from 'react-bootstrap';
 
 class GitHub extends Component {
 
     constructor() {
         super();
-        this.getGitHubData('tatenda')
+        this.getGitHubData('greg')
     }
 }
