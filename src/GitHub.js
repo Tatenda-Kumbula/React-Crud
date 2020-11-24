60d6ca8e5280d0d0905e73bc550d9ad44134b4ff
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-24 15:34:43 +0200

    Add files via upload

--- src/GitHub.js
+++ src/GitHub.js
@@ -54,3 +54,45 @@ class GitHub extends Component {
                 </Media.Body>
             </Media>                     
         );        
+
+        return (
+            <div>
+              <Form inline onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="formInlineName">                                    
+                      <Form.Control 
+                          type="text" 
+                          value={this.state.searchTerm} 
+                          placeholder="Enter Search Term" 
+                          onChange={this.handleChange}
+                      />
+                  </Form.Group>                            
+                  {' '}
+                  <Button type="submit">
+                      Search
+                  </Button>
+              </Form>                 
+  
+              <h3>GitHub Users Results</h3>
+              { this.state.isLoading &&                                             
+                  <ReactLoading type="spinningBubbles" color="#444" /> 
+              }  
+              {listUsers}                                                                                            
+            </div>
+          );
+      }
+      
+      handleSubmit(e) {
+          e.preventDefault();    
+          this.setState({
+              isLoading : true          
+          })                
+          this.getGitHubData(this.state.searchTerm);        
+      }
+  
+      handleChange(e) {
+          this.setState({ searchTerm: e.target.value });        
+      }
+  
+      
+  }
+  export default GitHub;
